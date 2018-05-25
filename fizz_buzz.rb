8a2bf7a9896db0be71eedfe2a60c9b221f65810d
Tracie <traciechang@hotmail.com>
2018-05-25 15:59:28 -0700

    add fizzbuzz

---
+++ fizz_buzz.rb
@@ -0,0 +1,43 @@
+# Write a program that outputs the string representation of numbers from 1 to n.
+
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+# Example:
+
+# n = 15,
+
+# Return:
+# [
+#     "1",
+#     "2",
+#     "Fizz",
+#     "4",
+#     "Buzz",
+#     "Fizz",
+#     "7",
+#     "8",
+#     "Fizz",
+#     "Buzz",
+#     "11",
+#     "Fizz",
+#     "13",
+#     "14",
+#     "FizzBuzz"
+# ]
+
+def fizz_buzz(n)
+    output = []
+    
+    for i in 1..n do
+        if i % 3 == 0 && i % 5 == 0
+            output << "FizzBuzz"
+        elsif i % 3 == 0
+            output << "Fizz"
+        elsif i % 5 == 0
+            output << "Buzz"
+        else
+            output << i.to_s
+        end
+    end
+    output
+end
