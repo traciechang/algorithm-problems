4391edd0ebbff65a67e8a70398d2dc11e92bd396
Tracie <traciechang@hotmail.com>
2018-06-03 16:25:09 -0700

    add length of last word

---
+++ length_of_last_word.rb
@@ -0,0 +1,16 @@
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+# If the last word does not exist, return 0.
+
+# Note: A word is defined as a character sequence consists of non-space characters only.
+
+# Example:
+
+# Input: "Hello World"
+# Output: 5
+
+def length_of_last_word(s)
+    last_word = s.split(" ").last
+    return 0 if !last_word
+    last_word.length
+end
