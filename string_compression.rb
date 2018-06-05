1aeee017f6ac7f1b0c87c10d359bb22d2728a056
Tracie <traciechang@hotmail.com>
2018-06-05 16:39:46 -0700

    add valid palindrome

--- string_compression.rb
+++ string_compression.rb
@@ -1,19 +1,36 @@
-def compress(chars)
-    hash = {}
-    arr = []
-    chars.each do |char|
-        if hash[char]
-            hash[char] +=1
-        else
-            hash[char] = 1
-        end
-    end
+# def compress(chars)
+#     hash = {}
+#     arr = []
+#     chars.each do |char|
+#         if hash[char]
+#             hash[char] +=1
+#         else
+#             hash[char] = 1
+#         end
+#     end
     
-    hash.each do |key, val|
-        arr << key
-        arr << val.to_s unless val == 1
+#     hash.each do |key, val|
+#         arr << key
+#         arr << val.to_s unless val == 1
+#     end
+#     arr
+# end
+
+# puts compress(["a","a","b","b","c","c","c"])
+
+require "pry"
+def convert_to_title(n)
+    alpha = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+    
+    num = n-1
+    q = num / 26 - 1
+    
+    if q == 0
+        return alpha[num]
+    else
+        # binding.pry
+        return alpha[q] + alpha[num % 26]
     end
-    arr
 end
 
-puts compress(["a","a","b","b","c","c","c"])
+puts convert_to_title(78)
