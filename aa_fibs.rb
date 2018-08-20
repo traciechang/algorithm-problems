26b489d7ddd885e5539dfdcfc3769e31e7a8769a
Tracie <traciechang@hotmail.com>
2018-08-20 10:36:08 -0700

    add fibs

---
+++ aa_fibs.rb
@@ -0,0 +1,26 @@
+# Write a function, fibs(num) which returns the first n elements from
+# the fibonnacci sequence, given n. Solve it both iteratively and recursively.
+
+def fibs(num)
+    arr = [0,1]
+    counter = 2
+    return [0] if num == 1
+
+    until counter == num
+        arr << arr[-1] + arr[-2]
+        counter +=1
+    end
+    arr
+end
+
+# print fibs(5)
+
+def fibs_recursive(num)
+    return [0] if num == 1
+    return [0,1] if num == 2
+
+    arr = fibs_recursive(num - 1)
+    arr + [arr[-1] + arr[-2]]
+end
+
+print fibs_recursive(5)
