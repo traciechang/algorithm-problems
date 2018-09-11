d42fd810640012ac5c807cfa5ebae1ac79d9d6c5
Tracie <traciechang@hotmail.com>
2018-09-11 14:56:35 -0700

    add bon appetit

---
+++ bon_appetit.rb
@@ -0,0 +1,17 @@
+def bonAppetit(bill, k, b)
+    annas_bill = 0
+
+    bill.each_with_index do |bil, idx|
+        annas_bill += bil if idx != k
+    end
+
+    annas_bill = annas_bill / 2
+    if annas_bill == b
+        print 'Bon Appetit'
+    else
+        print b - annas_bill
+    end
+end
+
+puts bonAppetit([3,10,2,9], 1, 12)
+# puts bonAppetit([3,10,2,9], 1, 7)
