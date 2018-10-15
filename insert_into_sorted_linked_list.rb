1c4274c7ea46ea0bdb3982bb9dfa26d9beaaa1b8
Tracie <traciechang@hotmail.com>
2018-10-15 10:36:56 -0700

    add intersection and union ll

--- insert_into_sorted_linked_list.rb
+++ insert_into_sorted_linked_list.rb
@@ -33,7 +33,7 @@ class LinkedList
             list2 << curr_node.val
             curr_node = curr_node.next_node
         end
-        binding.pry
+
         if list1 > list2
             1
         elsif list2 > list1
@@ -113,9 +113,49 @@ class LinkedList
     end
 end
 
-ll = LinkedList.new(nil)
-ll.insert(5)
-ll.insert(2)
+def intersection(list1, list2)
+    seen_hash = {}
+    curr_node_l1 = list1.head
+    curr_node_l2 = list2.head
+    new_list = LinkedList.new(nil)
+
+    until curr_node_l1.nil?
+        seen_hash[curr_node_l1.val] = true
+        curr_node_l1 = curr_node_l1.next_node
+    end
+
+    until curr_node_l2.nil?
+        new_list.insert(curr_node_l2.val) if seen_hash[curr_node_l2.val]
+        curr_node_l2 = curr_node_l2.next_node
+    end
+    new_list
+end
+
+def union(list1, list2)
+    seen_hash = {}
+    curr_node_l1 = list1.head
+    curr_node_l2 = list2.head
+    new_list = LinkedList.new(nil)
+
+    until curr_node_l1.nil?
+        seen_hash[curr_node_l1.val] = true
+        curr_node_l1 = curr_node_l1.next_node
+    end
+
+    until curr_node_l2.nil?
+        seen_hash[curr_node_l2.val] = true
+        curr_node_l2 = curr_node_l2.next_node
+    end
+
+    seen_hash.each do |k, v|
+        new_list.insert(k)
+    end
+    new_list
+end
+
+# ll = LinkedList.new(nil)
+# ll.insert(5)
+# ll.insert(2)
 # ll.insert(10)
 # ll.insert(7)
 # ll.insert(15)
@@ -136,11 +176,26 @@ ll.insert(2)
 # print ll.compare(list)
 
 # testing merge
-ll2 = LinkedList.new(nil)
-ll2.insert(3)
-ll2.insert(9)
-ll2.insert(10)
-ll.merge(ll2)
+# ll2 = LinkedList.new(nil)
+# ll2.insert(3)
+# ll2.insert(9)
+# ll2.insert(10)
+# ll.merge(ll2)
 
-print ll.return_list
-print ll2.return_list
+# print ll.return_list
+# print ll2.return_list
+
+# testing intersection & union
+ll1 = LinkedList.new(nil)
+ll1.insert(10)
+ll1.insert(4)
+ll1.insert(15)
+ll1.insert(20)
+ll2 = LinkedList.new(nil)
+ll2.insert(8)
+ll2.insert(4)
+ll2.insert(2)
+ll2.insert(10)
+# new_ll = intersection(ll1,ll2)
+new_ll = union(ll1, ll2)
+print new_ll.return_list
