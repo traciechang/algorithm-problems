4e16dbf03ffcc225823480d329040b6414f84997
Tracie <traciechang@hotmail.com>
2019-03-04 14:21:40 -0600

    redo self dividing nums

--- self_dividing_numbers.rb
+++ self_dividing_numbers.rb
@@ -15,4 +15,22 @@ def self_dividing_numbers(left, right)
     result
 end
 
+# 3/4/19
+def self_dividing_numbers(left, right)
+    output = []
+    
+    until left > right
+        temp = true
+        left.to_s.each_char do |char|
+            if char.to_i == 0 || left % char.to_i != 0
+                temp = false
+                break
+            end
+        end
+        output << left if temp
+        left +=1
+    end
+    output
+end
+
 puts self_dividing_numbers(1,22)
