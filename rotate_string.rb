f358d0e4da4f5093eea2f61fced67525071d2831
Tracie <traciechang@hotmail.com>
2019-03-06 12:10:01 -0600

    redo rotate str

--- rotate_string.rb
+++ rotate_string.rb
@@ -33,3 +33,23 @@ def rotate_string(a, b)
 end
 
 puts rotate_string("", "s")
+
+# 3/6/19
+
+def rotate_string(a, b)
+    return true if a == b
+    return false if a.length != b.length
+    max_shifts = a.length
+    shifts = 0
+    current_a = a
+    
+    until shifts > max_shifts
+        temp_word = ""
+        temp_word << current_a[1...max_shifts]
+        temp_word << current_a[0]
+        return true if temp_word == b
+        current_a = temp_word
+        shifts += 1
+    end
+    false
+end
