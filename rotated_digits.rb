eb6ae38ab5634f264289398e9decdc76c0ca9103
Tracie <traciechang@hotmail.com>
2019-03-06 11:55:38 -0600

    redo rotated digits

--- rotated_digits.rb
+++ rotated_digits.rb
@@ -14,35 +14,76 @@
 
 # N  will be in range [1, 10000].
 
+require 'pry'
+
+# def rotated_digits(n)
+#     good = {
+#         2 => 5,
+#         5 => 2,
+#         6 => 9,
+#         9 => 6
+#         }
+#     valid = {
+#         0 => 0,
+#         1 => 1,
+#         8 => 8
+#         }
+#     count = 0
+    
+#     (1..n).each do |num|
+#         is_possible = true
+#         is_good = false
+#         is_valid = false
+        
+#         num.to_s.each_char do |char|
+#             if !valid[char.to_i] && !good[char.to_i]
+#                 is_possible = false
+#                 break
+#             end
+            
+#             is_good = true if good[char.to_i]
+#             is_valid = true if valid[char.to_i]
+#         end
+#         count +=1 if (is_possible && is_good && is_valid) || (is_possible && is_good)
+#     end
+#     count
+# end
+
+# 3/6/19
+
 def rotated_digits(n)
-    good = {
-        2 => 5,
-        5 => 2,
-        6 => 9,
-        9 => 6
-        }
-    valid = {
+    hash = {
         0 => 0,
         1 => 1,
-        8 => 8
+        2 => 5,
+        3 => nil,
+        4 => nil,
+        5 => 2,
+        6 => 9,
+        7 => nil,
+        8 => 8,
+        9 => 6
         }
-    count = 0
     
-    (1..n).each do |num|
-        is_possible = true
-        is_good = false
-        is_valid = false
-        
-        num.to_s.each_char do |char|
-            if !valid[char.to_i] && !good[char.to_i]
-                is_possible = false
-                break
-            end
+    total = 0
+    i = 1
+    
+    until i > n
+        rotated_i = ""
+        valid = true
+        i.to_s.each_char do |char|
+            opp = hash[char.to_i]
             
-            is_good = true if good[char.to_i]
-            is_valid = true if valid[char.to_i]
+            if opp.nil?
+                valid = false
+            else
+                rotated_i << opp.to_s
+            end
         end
-        count +=1 if (is_possible && is_good && is_valid) || (is_possible && is_good)
+        total += 1 if valid  && rotated_i.to_i != i
+        i += 1
     end
-    count
+    total
 end
+
+puts rotated_digits(10)
