5cd08c8628d6dfeac61b70c0ef52154fde59315a
Tracie <traciechang@hotmail.com>
2019-03-19 10:49:57 -0500

    add remove duplicates from sorted arr

--- remove_duplicates_from_sorted_array.rb
+++ remove_duplicates_from_sorted_array.rb
@@ -1,5 +1,20 @@
 # Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
-Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-# def
+# 3/19/19
+
+def remove_duplicates(nums)
+    seen = {}
+    
+    nums.each_with_index do |num, idx|
+        if seen[num]
+            nums[idx] = nil
+        else
+            seen[num] = true
+        end
+    end
+    
+    nums.delete(nil)
+    nums.length
+end
