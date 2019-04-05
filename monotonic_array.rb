38733a07bda7254db62c240aa3ca3e159e4176ad
Tracie <traciechang@hotmail.com>
2019-04-05 15:26:36 -0500

    add monotonic array

--- monotonic_array.rb
+++ monotonic_array.rb
@@ -1,8 +1,8 @@
 # leetcode
 
-An array is monotonic if it is either monotone increasing or monotone decreasing.
+# An array is monotonic if it is either monotone increasing or monotone decreasing.
 
-An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+# An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
 
 Return true if and only if the given array A is monotonic. 
 
