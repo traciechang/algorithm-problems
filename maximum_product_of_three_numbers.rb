14561fc849d549d72ad493145e9a2c0bc56b831d
Tracie <traciechang@hotmail.com>
2019-05-02 13:19:23 -0500

    add max prod of 3 nums

--- maximum_product_of_three_numbers.rb
+++ maximum_product_of_three_numbers.rb
@@ -1,23 +1,23 @@
 # leetcode
 
-Given an integer array, find three numbers whose product is maximum and output the maximum product.
+# Given an integer array, find three numbers whose product is maximum and output the maximum product.
 
-Example 1:
+# Example 1:
 
-Input: [1,2,3]
-Output: 6
+# Input: [1,2,3]
+# Output: 6
  
 
-Example 2:
+# Example 2:
 
-Input: [1,2,3,4]
-Output: 24
+# Input: [1,2,3,4]
+# Output: 24
  
 
-Note:
+# Note:
 
-The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
-Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+# The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
+# Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
 
 # 5/2/19
 
