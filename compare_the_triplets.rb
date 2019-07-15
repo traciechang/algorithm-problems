29151cf6053a36d790756fab34b92716e55bb510
Tracie <traciechang@hotmail.com>
2019-07-15 15:57:11 -0500

    add comp triplets

--- compare_the_triplets.rb
+++ compare_the_triplets.rb
@@ -1,3 +1,17 @@
 # hackerrank
 
 # 7/15/19
+
+def compareTriplets(a, b)
+    alice = 0
+    bob = 0
+
+    a.each_with_index do |n, idx|
+        if n > b[idx]
+            alice += 1
+        elsif n < b[idx]
+            bob += 1
+        end
+    end
+    [alice,bob]
+end
