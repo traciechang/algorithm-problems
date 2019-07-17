20afbb44979b1aed46bbadbffc7a71fd197402fb
Tracie <traciechang@hotmail.com>
2019-07-17 16:29:49 -0500

    add first factorial

---
+++ first_factorial.rb
@@ -0,0 +1,16 @@
+# Coderbyte
+
+# 7/17/19
+
+def FirstFactorial(num)
+
+    # code goes here
+    n = 2
+    output = 1
+    
+    while n <= num
+      output *= n
+      n += 1
+    end
+    output      
+  end
