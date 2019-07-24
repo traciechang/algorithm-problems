8eff31388cfc2ac64692c7c63313d836bba8f689
Tracie <traciechang@hotmail.com>
2019-07-24 14:53:14 -0500

    add time convert

---
+++ check_nums.rb
@@ -0,0 +1,14 @@
+# coderbyte
+
+# 7/24/19
+
+def CheckNums(num1,num2)
+    if num2 > num1
+        true
+    elsif num1 > num2
+        false
+    else
+        -1
+    end
+     
+end
---
+++ time_convert.rb
@@ -0,0 +1,11 @@
+# coderbyte
+
+# 7/24/19
+
+def TimeConvert(num)
+    hours = num / 60
+    min = num % 60
+    
+    "#{hours}" + ":" + "#{min}"
+         
+end
