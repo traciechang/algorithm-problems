c8b4bd4baeec335a30e2e214119f0435627792ce
Tracie <traciechang@hotmail.com>
2019-08-07 13:43:57 -0500

    add find dup num

---
+++ find_the_duplicate_number.rb
@@ -0,0 +1,11 @@
+def find_duplicate(nums)
+    hash = {}
+    
+    nums.each do |n|
+        hash[n] = hash[n] ? hash[n] + 1 : 1
+    end
+    
+    hash.each do |key, val|
+        return key if val != 1
+    end
+end
